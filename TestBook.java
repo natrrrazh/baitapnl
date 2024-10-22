2076aab55475105cfe3aa61724aa623126a614a6
natrrrazh <hatranbaotran2512@gmail.com>
2024-10-22 13:56:47 +0700

    init project

--- TestBook.java
+++
@@ -1,8 +0,0 @@
-public class TestBook {
-    public static void main(String[] args) {
-        // Tạo một đối tượng Book
-        Book myBook = new Book("example@email.com", 'm');
-        System.out.println("Email: " + myBook.getEmail());
-        System.out.println("Gender: " + myBook.getGender());
-    }
-}
